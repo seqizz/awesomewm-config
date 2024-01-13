2985a827fa3ce168c82960c2d32d7a8689f0a0ee
Gürkan <gitoperations@gurkan.in>
2024-01-13 15:41:47 +0100

    Also restrict layouts for the mail tag

--- my_modules/rc_tags.lua
+++ my_modules/rc_tags.lua
@@ -1,41 +1,30 @@
-local awful = require("awful")
+local awful = require('awful')
 local l = awful.layout.suit
 
 -- create tags
-tag_web = awful.tag.add(
-  "web",
-  {
-    layout             = l.max,
-    layouts            = { l.max, l.tile },
-    gap_single_client  = false,
-    gap                = 4,
-    selected           = true,
-  }
-)
-if hostname == "innixos" or hostname == "innodellix" then
-  tag_mail = awful.tag.add(
-    "mail",
-    {
-      layout = l.max,
-    }
-  )
+tag_web = awful.tag.add('web', {
+  layout = l.max,
+  layouts = { l.max, l.tile },
+  gap_single_client = false,
+  gap = 4,
+  selected = true,
+})
+if hostname == 'innodellix' then
+  tag_mail = awful.tag.add('mail', {
+    layout = l.max,
+    layouts = { l.max, l.tile },
+  })
 end
-tag_term = awful.tag.add(
-  "term",
-  {
-    layout             = l.max,
-    layouts            = { l.max, l.tile },
-    gap_single_client  = false,
-    gap                = 4,
-  }
-)
-tag_chat = awful.tag.add(
-  "chat",
-  {
-    layout              = l.max,
-    layouts             = { l.max, l.tile },
-    gap_single_client   = false,
-    gap                 = 4,
-    master_width_factor = 0.5,
-  }
-)
+tag_term = awful.tag.add('term', {
+  layout = l.max,
+  layouts = { l.max, l.tile },
+  gap_single_client = false,
+  gap = 4,
+})
+tag_chat = awful.tag.add('chat', {
+  layout = l.max,
+  layouts = { l.max, l.tile },
+  gap_single_client = false,
+  gap = 4,
+  master_width_factor = 0.5,
+})
