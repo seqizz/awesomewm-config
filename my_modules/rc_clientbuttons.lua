b1d49fc7706f07d36b81bbf48d3efb2f45e73f17
Gürkan <gitoperations@gurkan.in>
2026-01-06 11:14:12 +0100

    Add Win-Alt-Rclick for resizing fake screens

--- my_modules/rc_clientbuttons.lua
+++ my_modules/rc_clientbuttons.lua
@@ -47,5 +47,58 @@ clientbuttons = awful.util.table.join(
   awful.button({ win, alt }, 1, function(c)
     c:emit_signal('request::activate', 'mouse_click', { raise = true })
     awful.mouse.client.resize(c)
+  end),
+  -- modkey + Alt + Right Click drag to resize fake screen boundary
+  awful.button({ win, alt }, 3, function(c)
+    c:emit_signal('request::activate', 'mouse_click', { raise = true })
+    local s = mouse.screen
+    local start_x = mouse.coords().x
+
+    -- Find screen info in screens_table
+    local fake_screen = nil
+    local parent_screen = nil
+    for name, properties in pairs(screens_table) do
+      if properties["object"] == s then
+        if properties["is_fake"] then
+          fake_screen = s
+          parent_screen = properties["parent"]["object"]
+        else
+          -- Check if this screen has a fake child
+          for n2, p2 in pairs(screens_table) do
+            if p2["is_fake"] and p2["parent"]["object"] == s then
+              fake_screen = p2["object"]
+              parent_screen = s
+              break
+            end
+          end
+        end
+        break
+      end
+    end
+
+    if not fake_screen or not parent_screen then
+      return -- No fake screen pair found
+    end
+
+    local fake_start_geo = fake_screen.geometry
+    local parent_start_geo = parent_screen.geometry
+
+    mousegrabber.run(function(_mouse)
+      if _mouse.buttons[3] then
+        local diff = _mouse.x - start_x
+        -- Fake screen is on the left, parent on the right
+        -- Moving mouse right = grow fake, shrink parent
+        local new_fake_width = fake_start_geo.width + diff
+        local new_parent_width = parent_start_geo.width - diff
+
+        -- Minimum width sanity check
+        if new_fake_width > 200 and new_parent_width > 200 then
+          fake_screen:fake_resize(fake_start_geo.x, fake_start_geo.y, new_fake_width, fake_start_geo.height)
+          parent_screen:fake_resize(parent_start_geo.x + diff, parent_start_geo.y, new_parent_width, parent_start_geo.height)
+        end
+        return true
+      end
+      return false
+    end, 'sb_h_double_arrow')
   end)
 )
